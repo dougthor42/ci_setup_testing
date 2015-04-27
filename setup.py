31ebe422b8acdd2ac615ea2cf1c3688b0b4a10b8
Douglas Thor <dougthor42@users.noreply.github.com>
2015-04-27 14:23:44 -0700

    Added setup.py for AppVeyor - built to whl.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'ci_setup_check',
+    version = '1.0',
+    description = """This is a demo package used for figuring out travics-ci
+ and AppVeyor""",
+    packages=['ci_setup_check', 'tests'],
+)
