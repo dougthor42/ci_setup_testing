50b553a1fe96f2d5e3b988875e140ef0649c9b16
Douglas Thor <dougthor42@users.noreply.github.com>
2015-04-29 16:30:54 -0700

    Playing around with import statements.

--- ci_setup_check/tests/test_core.py
+++ ci_setup_check/tests/test_core.py
@@ -1,12 +1,22 @@
 from __future__ import print_function, division
-from __future__ import absolute_import
+#from __future__ import absolute_import
 import unittest
-#import ci_setup_check
+
+# NoseTest OK/Fail
+### Absolute import Y/N                  N               Y
+# -------------------------------------------------------------
+import ci_setup_check.core as core     # OK              OK
+#from .. import core as core            # OK              OK
+#from . import core as core             # fail            fail
+#from ci_setup_check import core        # OK              OK
+#import core                            # fail           fail
+
 
 
 class Examples(unittest.TestCase):
     def test_things(self):
-        self.assertEqual(5, 5)
+        result = core.func(2, 3)
+        self.assertEqual(result, 5)
 
 
 if __name__ == "__main__":
