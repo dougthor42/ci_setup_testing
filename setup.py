ca49ba485d05e7493c6e91b741fc0998d555ea87
Douglas Thor <doug.thor@gmail.com>
2017-02-06 13:03:48 -0800

    only deploy on tags. version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'ci_setup_check',
-    version = '1.0.1',
+    version = '1.0.2',
     description = """This is a demo package used for figuring out travics-ci
  and AppVeyor""",
     packages=['ci_setup_check', 'ci_setup_check.tests'],
